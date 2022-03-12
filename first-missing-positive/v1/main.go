c6d87e691e239e5156ab31402d6588796bd6b4cc
Tom M G <devbytom@gmail.com>
2022-03-12 16:22:21 -0300

    Solve first-missing-positive-integer

--- first-missing-positive/v1/main.go
+++ first-missing-positive/v1/main.go
@@ -2,51 +2,36 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func firstMissingPositive(nums []int) int {
-	positives := []int{}
-	for _, current := range nums {
-		if current > 0 {
-			positives = append(positives, current)
+	arraySize := len(nums)
+	for _, num := range nums {
+		// Move only positive integers to their own index (value-1)
+		for num > 0 && num-1 < arraySize {
+			tmp := nums[num-1]
+			nums[num-1] = num
+			if tmp == num {
+				break
+			}
+			num = tmp
 		}
 	}
-	if len(positives) < 1 {
-		return 1
-	}
 
-	sort.Ints(positives)
-
-	size := len(positives)
-	if size == 1 {
-		return positives[0:1][0] + 1
-	}
-
-	var max int
-	for k, current := range positives {
-		if k == 0 {
-			continue
+	// Scan again and find any value-1 that doesn't match the index
+	for i, num := range nums {
+		if i != num-1 {
+			return i + 1
 		}
-
-		previous := positives[k-1 : k][0]
-		if current == previous {
-			continue
-		}
-
-		if current-previous > 1 {
-			return previous + 1
-		}
-
-		max = current
-		continue
 	}
-
-	return max + 1
+	return arraySize + 1
 }
 
 func main() {
-	fmt.Println("Result:", firstMissingPositive([]int{-1, -3}))
-	fmt.Println("Result:", firstMissingPositive([]int{1, 200, 3, 5, 8, 7, 6, 4, 1, 2}))
-	fmt.Println("Result:", firstMissingPositive([]int{-9, 100, 1, 3, 6, 4, 1, 2}))
+	// fmt.Println("Result:", firstMissingPositive([]int{-1, -3}))
+	// fmt.Println("Result:", firstMissingPositive([]int{1, 1}))
+	// fmt.Println("Result:", firstMissingPositive([]int{1, 2, 0}))
+	fmt.Println("Result:", firstMissingPositive([]int{3, 4, -1, 1}))
+	// fmt.Println("Result:", firstMissingPositive([]int{1, 200, 3, 5, 8, 7, 6, 4, 1, 2}))
+	// fmt.Println("Result:", firstMissingPositive([]int{-9, 100, 1, 3, 6, 4, 1, 2}))
 }
--- first-missing-positive/v1/main_test.go
+++ first-missing-positive/v1/main_test.go
@@ -11,8 +11,12 @@ func TestFirstMissingPositive(t *testing.T) {
 		expected int
 	}{
 		{"Should return 1 where there is only negative numbers", []int{-3, -2, -1}, 1},
-		{"Should return 7 when input is [4,5,6,8]", []int{4, 5, 6, 8}, 7},
-		{"Should return 200 when input is [197,198,199]", []int{197, 198, 199}, 200},
+		{"Should return 1 when input is [4,5,6,8]", []int{4, 5, 6, 8}, 1},
+		{"Should return 2 when input is [1,4,5,6,8]", []int{1, 4, 5, 6, 8}, 2},
+		{"Should return 2 when input is [3,4,-1,1]", []int{3, 4, -1, 1}, 2},
+		{"Should return 2 when input is [1,1]", []int{1, 1}, 2},
+		{"Should return 3 when input is [1,1,4]", []int{1, 2, 0}, 3},
+		{"Should return 5 when input is [1,2,3,4,197,198,199]", []int{1, 2, 3, 4, 197, 198, 199}, 5},
 	}
 
 	for _, tc := range cases {
